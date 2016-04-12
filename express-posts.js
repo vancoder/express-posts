f714f2568d5c8a56497fddace55d1388c888acfe
Grant Mangham <grant@10up.com>
2016-04-12 15:41:26 -0700

    correct formatting

--- express-posts.js
+++ express-posts.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function($) {
-    $('.express-posts-relationship').live('change', function() {
-        var r = $(this).val();
-        $(this).parents('form').find('fieldset').hide();
-        $(this).parents('form').find('fieldset.' + r).show();
-    });
-});
+jQuery( document ).ready( function ( $ ) {
+	$( '.express-posts-relationship' ).live( 'change', function () {
+		var r = $( this ).val();
+		$( this ).parents( 'form' ).find( 'fieldset' ).hide();
+		$( this ).parents( 'form' ).find( 'fieldset.' + r ).show();
+	} );
+} );
